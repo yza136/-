d6473b1fcdebbd63d30d8696b1f335516ff3e0cd
yza136 <48724289+yza136@users.noreply.github.com>
2019-09-27 01:19:37 +0800

    Add files via upload

---
+++ ces.js
@@ -0,0 +1,45 @@
+
+
+var json={
+	A1:{v:5,i:99},
+	A2:2323
+}
+
+
+	for(let i=0;i<5;i++){
+		var e='E';
+		var  Q= e+i;
+		console.log(Q);
+		json.newParam =Q;
+		
+		json['E'+i]={'A':{m:5656,bb:65656}}
+		console.log(json);
+		console.log('————————————————————————')
+	}
+
+
+
+
+// var json = [];
+// 
+// for (var i = 0; i < names.length; i++) {
+// 
+// json[i] = {'name' : 4444};
+// }
+// 
+
+
+
+
+
+
+// var json=[
+// 	{'A1':65656},
+// 	{'A2':2323}
+// ]
+// 	for(let i=0;i<5;i++){
+// 		json.push({'A':{m:5656,bb:65656}})
+// 		console.log(json);
+// 		console.log('————————————————————————')
+// 	}
+// 
---
+++ new.js
@@ -0,0 +1,80 @@
+const
+	fs = require("fs"),
+	xlsx = require("xlsx")
+    cheerio = require('cheerio'),
+	request = require('superagent');
+
+class Handle {
+	excle() {
+		let
+			i,
+			x,
+			url,
+			urlArr = [],
+			realm,
+			url1 = [],
+			know = 'know.baidu.com',
+			RemoveReg = '/展开全部/g',
+			leaf = /叶黄素/g,
+			wenda = 'wenda.so.com',
+			ask = 'ask.360kad.com',
+			wukong = 'www.wukong.com',
+			sogou = 'wenwen.sogou.com',
+			iask = 'iask.sina.com.cn',
+			kuaiwen = 'kuaiwen.pcbaby.com.cn',
+			tieba = 'tieba.baidu.com',
+			realmReg = /[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+\.?/,
+			rangeReg = /[0-9]+$/g,
+			workbook = xlsx.readFile('file.xlsx'),
+			sheetName = workbook.SheetNames,
+			sheet1 = workbook.Sheets[sheetName[0]],
+			arr = xlsx.utils.sheet_to_json(sheet1, {
+				header: 1
+			}),
+			ws = xlsx.utils.aoa_to_sheet(arr),
+			ref = sheet1['!ref'],
+			range = ref.match(rangeReg)[0];
+          console.log(ws)
+
+		for (i = 2; i < range; i++) {
+			url = ws['E' + i].v,
+			realm = url.match(realmReg)[0];
+			if (realm == know) {
+				let c=url,
+				    k=i;
+				
+				request.get(url)
+					.end((err, res) => {
+						if (!err) {
+							let
+								a = 1,
+								html = res.text,
+								$ = cheerio.load(html, {
+									decodeEntities: true
+								}),
+							$itemMod = $('.answer-content').find('.con'),
+							context = $itemMod.text();
+							
+							if (leaf.test(context)) {
+								// console.log(context + k)
+								// console.log('————————————————————————')
+								// console.log(c)
+							} else {
+
+							}
+						}
+					})
+			}
+
+		};
+
+
+
+	}
+	read() {
+
+	}
+}
+var a = new Handle()
+a.excle()
+a.read()
